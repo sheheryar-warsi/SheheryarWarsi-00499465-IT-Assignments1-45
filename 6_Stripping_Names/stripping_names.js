98c57f12ea5e2a5537d4d659fdd3eeaf63529b9e
sheheryar-warsi <142478813+sheheryar-warsi@users.noreply.github.com>
2024-02-20 22:34:01 +0500

    Delete 6_Stripping_Names directory

--- 6_Stripping_Names/stripping_names.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const PERSON_NAME = '   \n\tSheheryar Warsi\t\n';
-console.log(PERSON_NAME);
-const NewName = PERSON_NAME.trim();
-console.log(NewName);
--- 6_Stripping_Names/stripping_names.ts
+++
@@ -1,7 +0,0 @@
-const PERSON_NAME : string = '   \n\tSheheryar Warsi\t\n';
-
-console.log(PERSON_NAME);
-
-const NewName :string = PERSON_NAME.trim();
-
-console.log(NewName);
