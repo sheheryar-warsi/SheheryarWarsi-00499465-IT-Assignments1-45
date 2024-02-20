54ee6dc34ac868fba8e5f27133174b696a1c9a8f
sheheryar-warsi <142478813+sheheryar-warsi@users.noreply.github.com>
2024-02-20 22:34:53 +0500

    Delete 4_Famous_Quote directory

--- 4_Famous_Quote/famous_quote.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-console.log('Quaid-e-Azam once said,"I do not believe in taking the right decision, I take a decision and make it right."');
--- 4_Famous_Quote/famous_quote.ts
+++
@@ -1,1 +0,0 @@
-console.log('Quaid-e-Azam once said,"I do not believe in taking the right decision, I take a decision and make it right."');
