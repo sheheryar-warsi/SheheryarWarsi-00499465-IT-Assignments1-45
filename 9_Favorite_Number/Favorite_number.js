dd65ab164589ba139b6c7c096b981b6a12d47638
sheheryar-warsi <142478813+sheheryar-warsi@users.noreply.github.com>
2024-02-20 22:33:31 +0500

    Delete 9_Favorite_Number directory

--- 9_Favorite_Number/Favorite_number.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-let GPA = 3.8;
-console.log(`I Achieved ${GPA} CGPA in three semesters.`);
--- 9_Favorite_Number/Favorite_number.ts
+++
@@ -1,3 +0,0 @@
-let GPA :number = 3.8;
-
-console.log(`I Achieved ${GPA} CGPA in three semesters.`);
