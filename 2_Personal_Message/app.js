b6d13825bf801c7a81b8d2d2985653a3530ff86d
sheheryar-warsi <142478813+sheheryar-warsi@users.noreply.github.com>
2024-02-20 22:34:27 +0500

    Delete 2_Personal_Message directory

--- 2_Personal_Message/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let PersonName = '';
-PersonName = prompt('What is your name?') || '';
-if (PersonName !== null && PersonName !== '') {
-    alert(`Hello ${PersonName},would you like to learn some python today?`);
-}
-else {
-alert(`Please fill up your name.`);
-}
