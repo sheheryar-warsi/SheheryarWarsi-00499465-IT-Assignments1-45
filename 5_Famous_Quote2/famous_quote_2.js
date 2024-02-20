75a049b38348e0fa5a5951c8dd3907a064175456
sheheryar-warsi <142478813+sheheryar-warsi@users.noreply.github.com>
2024-02-20 22:34:14 +0500

    Delete 5_Famous_Quote2 directory

--- 5_Famous_Quote2/famous_quote_2.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let FamousPerson = 'Quaid-e-Azam';
-let Message = '"I do not believe in taking the right decision, I take a decision and make it right."';
-console.log(`${FamousPerson} once said,${Message}`);
--- 5_Famous_Quote2/famous_quote_2.ts
+++
@@ -1,3 +0,0 @@
-let FamousPerson :string = 'Quaid-e-Azam';
-let Message :string = '"I do not believe in taking the right decision, I take a decision and make it right."';
-console.log(`${FamousPerson} once said,${Message}`);
